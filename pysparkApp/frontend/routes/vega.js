5f4f6feafa642d95f7c274f6a0dd70f5d5454d43
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2019-12-11 14:42:27 +0100

    Set up handlebars layouts, partials, and helpers
    Also formatted all files to be pretty

--- pysparkApp/frontend/routes/vega.js
+++ pysparkApp/frontend/routes/vega.js
@@ -4,12 +4,12 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('vega', { title: 'Vega Visualization', layout:'layout-vega' });
+router.get('/', function (req, res, next) {
+    res.render('vega', {title: 'Vega Visualization', layout: 'layout-vega'});
 });
 
-router.get('/choro', function(req, res, next) {
-  res.render('vega_choro', { title: 'Choropleth Visualization', layout:'layout-vega' });
+router.get('/choro', function (req, res, next) {
+    res.render('vega_choro', {title: 'Choropleth Visualization', layout: 'layout-vega'});
 });
 
 module.exports = router;
---
+++ pysparkApp/frontend/src/handlebarsHelpers.js
@@ -0,0 +1,8 @@
+helpers = {
+    //Add handlebars helpers here as functions, eg:
+    toCaps: function (value) {
+        return value.toUpperCase()
+    }
+};
+
+exports.helpers = helpers;
