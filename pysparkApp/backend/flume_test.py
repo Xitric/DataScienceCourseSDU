59e927242a7a47451a98f93259b1dfce17169f65
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2019-12-11 13:32:39 +0100

    Temporary commit, removed one sink, and Streaming is broken

--- pysparkApp/backend/flume_test.py
+++ pysparkApp/backend/flume_test.py
@@ -1,15 +1,31 @@
-from pyspark import SparkContext, SparkConf
-from pyspark.streaming import StreamingContext
+from pyspark import SparkContext, SparkConf, RDD
+from pyspark.streaming import StreamingContext, DStream
 from pyspark.streaming.flume import FlumeUtils
 from pyspark.sql import SQLContext
 import json
 
-def handleRDD(rdd):
+
+def get_service_dstream() -> DStream:
+    return FlumeUtils.createPollingStream(ssc, [("flume", 4000)])
+
+
+def get_incident_dstream() -> DStream:
+    return FlumeUtils.createPollingStream(ssc, [("flume", 4001)])
+
+
+def handle_service_case(rdd: RDD):
     if not rdd.isEmpty():
-        rdd.toDF().show(10, True)
+        rdd.toDF().show(10, False)
+
+
+def handle_incident_report(rdd: RDD):
+    if not rdd.isEmpty():
+        rdd.toDF().show(10, False)
+
 
 if __name__ == "__main__":
-    # Create a local StreamingContext with two working thread and batch interval of 1 second
+    # TODO: Can we use a spark session instead? That way we are not using different techniques in different places
+    # Create a local StreamingContext with two working threads and batch interval of 1 second
     conf = SparkConf() \
         .set('spark.driver.host', '127.0.0.1') \
         .set("spark.jars.packages", "org.apache.spark:spark-streaming-flume_2.11:2.4.4")
@@ -18,13 +34,16 @@ if __name__ == "__main__":
     sql = SQLContext(sc)
     print(sc.version)
 
-    # Configure input DStream for Flume
-    flumeStream = FlumeUtils.createStream(ssc, "pyspark", 4444)
-    # flumeStream.map(lambda rdd: json.loads(rdd[1])).pprint()
-    flumeStream.map(lambda rdd: json.loads(rdd[1])).foreachRDD(handleRDD)
+    # Configure input DStreams for Flume
+    get_service_dstream().map(lambda rdd: json.loads(rdd[1])).foreachRDD(handle_service_case)
+    get_incident_dstream().map(lambda rdd: json.loads(rdd[1])).foreachRDD(handle_incident_report)
+
+    # Process data
+    # flumeStream.map(lambda rdd: json.loads(rdd[1])).foreachRDD(handleRDD)
+    # serviceStream.map(lambda rdd: json.loads(rdd[1])).foreachRDD(handle_service_case)
+    # policeStream.map(lambda rdd: json.loads(rdd[1])).foreachRDD(handle_incident_report)
 
     # Run for 2 minutes
     ssc.start()
     ssc.awaitTerminationOrTimeout(120)
     ssc.stop()
-    # TODO: Json deserializer? The serialize data to json on the source?
