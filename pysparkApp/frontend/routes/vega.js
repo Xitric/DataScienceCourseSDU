e71fd395fd2c4f3ead3e348993fb217704a831b1
Casper Hahn <casperhahn@hotmail.com>
2019-12-12 13:35:38 +0100

    Added horizon graph

--- pysparkApp/frontend/routes/vega.js
+++ pysparkApp/frontend/routes/vega.js
@@ -5,11 +5,15 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('vega', {title: 'Vega Visualization', layout: 'layout-vega'});
+    res.render('vega', {title: 'Vega Visualization', script: 'vega_vis', layout: 'layout-vega'});
 });
 
 router.get('/choro', function (req, res, next) {
-    res.render('vega_choro', {title: 'Choropleth Visualization', layout: 'layout-vega'});
+    res.render('vega', {title: 'Choropleth Visualization', script: 'vega_vis_choro', layout: 'layout-vega'});
+});
+
+router.get('/horizon', function (req, res, next) {
+    res.render('vega_horizon', {title: 'Horizon Visualization', script: 'vega_vis_horizon', layout: 'layout-vega'});
 });
 
 module.exports = router;
