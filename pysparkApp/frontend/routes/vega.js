4152a3e0dc759e2959a2c67d967056b9227f2859
Casper Hahn <casperhahn@hotmail.com>
2019-12-12 17:48:48 +0100

    Added dropdown with filters

--- pysparkApp/frontend/routes/vega.js
+++ pysparkApp/frontend/routes/vega.js
@@ -5,15 +5,15 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('vega', {title: 'Vega Visualization', script: 'vega_vis', layout: 'layout-vega'});
+    res.render('vega', {title: 'Vega Visualization', script: 'vega_vis', layout: 'layout_vega'});
 });
 
 router.get('/choro', function (req, res, next) {
-    res.render('vega', {title: 'Choropleth Visualization', script: 'vega_vis_choro', layout: 'layout-vega'});
+    res.render('vega', {title: 'Choropleth Visualization', script: 'vega_vis_choro', layout: 'layout_vega'});
 });
 
 router.get('/horizon', function (req, res, next) {
-    res.render('vega_horizon', {title: 'Horizon Visualization', script: 'vega_vis_horizon', layout: 'layout-vega'});
+    res.render('vega_horizon', {title: 'Horizon Visualization', script: 'vega_vis_horizon', layout: 'layout_vega', data: '../public/dataset/neighborhoods.txt'});
 });
 
 module.exports = router;
--- pysparkApp/frontend/src/handlebarsHelpers.js
+++ pysparkApp/frontend/src/handlebarsHelpers.js
@@ -1,7 +1,16 @@
+var fs = require('fs');
+
 helpers = {
     //Add handlebars helpers here as functions, eg:
     toCaps: function (value) {
         return value.toUpperCase()
+    },
+
+    readData: function (file) {
+        var contents = fs.readFileSync(file, 'utf-8')
+        contents = contents.split(',');
+
+        return contents;
     }
 };
 
