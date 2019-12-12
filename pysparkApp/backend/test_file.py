51d732a12c5774380bc783e56b7bfccc7e3a0517
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2019-12-12 12:23:57 +0100

    Managed to execute a Spark job in client mode

--- pysparkApp/backend/test_file.py
+++ pysparkApp/backend/test_file.py
@@ -1,9 +1,10 @@
 from pyspark.sql.session import SparkSession
 
 spark = SparkSession.builder \
-    .config("spark.jars", "/backend/shc-core-1.1.3-2.4-s_2.11-jar-with-dependencies.jar") \
+    .appName("SfDataImporter") \
+    .master("yarn") \
+    .config("spark.submit.deployMode", "client") \
     .config("spark.driver.host", "172.200.0.55") \
     .config("spark.driver.port", "43345") \
-    .appName("SfDataImporter") \
     .getOrCreate()
 print(spark.sparkContext.parallelize([1,2,3]).count())
