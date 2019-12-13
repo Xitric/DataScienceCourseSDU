872337939e82b348c924987d702e648e14749eaf
Emil Nielsen <emilqz@gmail.com>
2019-12-13 08:49:45 +0100

    Added sh files

--- pysparkApp/run.sh
+++ pysparkApp/run.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-spark-submit --master yarn --deploy-mode client /backend/test_file.py
+docker build --rm -f "pysparkApp/Dockerfile" -t pysparkapp:latest pysparkApp && docker run --rm --ip 172.200.0.55 --hostname pyspark --network hadoop --env-file hadoop.env pysparkapp:latest
---
+++ pysparkApp/submit.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+spark-submit --master yarn --deploy-mode client /backend/data_importer.py
