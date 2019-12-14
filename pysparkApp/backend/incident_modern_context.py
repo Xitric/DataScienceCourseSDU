7059bb427e37b127178321e816aa573c383ca0ba
Emil Nielsen <emilqz@gmail.com>
2019-12-14 11:23:53 +0100

    HBase fix

--- pysparkApp/backend/incident_modern_context.py
+++ pysparkApp/backend/incident_modern_context.py
@@ -3,10 +3,10 @@ import os
 from geo_pyspark.sql.types import GeometryType
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import udf, unix_timestamp, to_timestamp
-from pyspark.sql.types import BooleanType, DoubleType
+from pyspark.sql.types import BooleanType, DoubleType, IntegerType
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
-
+from string_hasher import string_hash
 from context import Context
 
 
@@ -20,6 +20,7 @@ def create_polygon(multipolygon_string):
         points.append(Point(float(coordinates[1]), float(coordinates[0])))
     return Polygon([[p.x, p.y] for p in points])
 
+
 multipolygon = udf(
     lambda multipolygon_string: create_polygon(multipolygon_string), GeometryType()
 )
@@ -31,6 +32,17 @@ check_neighborhood_in_polygon = udf(
     BooleanType()
 )
 
+analysis_neighborhood_id = udf(
+    lambda analysis_neighborhood_str: string_hash(analysis_neighborhood_str),
+    IntegerType()
+)
+
+incident_category_id = udf(
+    lambda incident_category_str: string_hash(incident_category_str),
+    IntegerType()
+)
+
+
 class IncidentModernContext(Context):
     # File from HDFS
     incident_modern_file = os.environ["CORE_CONF_fs_defaultFS"] \
@@ -41,14 +53,18 @@ class IncidentModernContext(Context):
             "table":{"namespace:"default", "name":incidents_modern"},
             "rowkey":"key_analysis_neighborhood:key_incident_category:key_incident_datetime:key_row_id",
             "columns":{
-                "analysis_neighborhood":{"cf":"rowkey", "col":"key_analysis_neighborhood", "type":"int"},
-                "incident_category":{"cf":"rowkey", "col":"key_incident_category", "type":"int"},
+                "analysis_neighborhood_id":{"cf":"rowkey", "col":"key_analysis_neighborhood", "type":"int"},
+                "incident_category_id":{"cf":"rowkey", "col":"key_incident_category", "type":"int"},
                 "incident_datetime":{"cf":"rowkey", "col":"key_incident_datetime", "type":"int"},
                 "row_id":{"cf":"rowkey", "col":"key_row_id", "type":"int},
                 
                 "analysis_neighborhood":{"cf":"a", "col":"opened", "type":"string"},
                 "incident_category":{"cf":"a", "col":"incident_category", "type":"string"},
                 "incident_subcategory":{"cf":"a", "col":"incident_subcategory", "type":"string"},
+                "incident_datetime":{"cf":"a", "col":"incident_datetime", "type":"int"},
+                "report_datetime":{"cf":"a", "col":"report_datetime", "type":"int"},
+                "incident_id":{"cf":"a", "col":"incident_id", "type":"string"},
+                "resolution":{"cf":"a", "col":"resolution", "type":"string"},
                 
                 "intersection":{"cf":"l", "col":"intersection", "type":"string"},
                 "latitude":{"cf":"l", "col":"latitude", "type":"double"},            
@@ -59,7 +75,7 @@ class IncidentModernContext(Context):
                 "incident_number":{"cf":"m", "col":"incident_number", "type":"string"},
                 "police_district":{"cf":"m", "col":"police_district", "type":"string"},
                 "supervisor_district":{"cf":"m", "col":"supervisor_district", "type":"string"},
-                
+              }  
     }""".split())
 
     def load_csv(self, session: SparkSession) -> DataFrame:
@@ -92,21 +108,25 @@ class IncidentModernContext(Context):
 
         # Select the relevant columns
         incidents_modern_df = incidents_modern_df.select(
+            incidents_modern_df["Row ID"].alias("row_id"),
+
+            incidents_modern_df["Incident Category"].alias("incident_category"),
+            incident_category_id("Incident Category").alias("incident_category_id"),
+            incidents_modern_df["Incident Subcategory"].alias("incident_subcategory"),
             unix_timestamp(to_timestamp("Incident Datetime", "yyyy/MM/dd hh:mm:ss a")).alias("incident_datetime"),
             unix_timestamp(to_timestamp("Report Datetime", "yyyy/MM/dd hh:mm:ss a")).alias("report_datetime"),
-            incidents_modern_df["Row ID"].alias("row_id"),
             incidents_modern_df["Incident ID"].alias("incident_id"),  # Docs: Plain text, seems to contain numbers only
-            incidents_modern_df["Incident Number"].alias("incident_number"),  # Text, seems to contain numbers only
+            incidents_modern_df["Resolution"].alias("resolution"),
+
+            incidents_modern_df["Intersection"].alias("intersection"),
+            incidents_modern_df["Latitude"].cast(DoubleType()).alias("latitude"),
+            incidents_modern_df["Longitude"].cast(DoubleType()).alias("longitude"),
+
             incidents_modern_df["Report Type Code"].alias("report_type_code"),
             incidents_modern_df["Report Type Description"].alias("report_type_description"),
-            incidents_modern_df["Incident Category"].alias("incident_category"),
-            incidents_modern_df["Incident Subcategory"].alias("incident_subcategory"),
-            incidents_modern_df["Resolution"].alias("resolution"),
-            incidents_modern_df["Intersection"].alias("intersection"),
+            incidents_modern_df["Incident Number"].alias("incident_number"),  # Text, seems to contain numbers only
             incidents_modern_df["Police District"].alias("police_district"),  # Text, seems to contain numbers only
             incidents_modern_df["Supervisor District"].alias("supervisor_district"),
-            incidents_modern_df["Latitude"].cast(DoubleType()).alias("latitude"),
-            incidents_modern_df["Longitude"].cast(DoubleType()).alias("longitude")
         )
         incidents_modern_df.show(10, True)
 
@@ -117,6 +137,8 @@ class IncidentModernContext(Context):
             "cross"
         )
         incidents_modern_df = incidents_modern_df.drop("polygon")
+        incidents_modern_df = incidents_modern_df.withColumn("analysis_neighborhood_id", analysis_neighborhood_id(
+            incidents_modern_df["analysis_neighborhood"]))
         incidents_modern_df.show(10, True)
 
         return incidents_modern_df
