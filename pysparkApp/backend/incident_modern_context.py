1c9b9372d651e9fafc2a100ddc5113cbb120bc73
Emil Nielsen <emilqz@gmail.com>
2019-12-15 11:22:58 +0100

    Clean up: comments, naming, and timestamp fix

--- pysparkApp/backend/incident_modern_context.py
+++ pysparkApp/backend/incident_modern_context.py
@@ -10,10 +10,10 @@ from string_hasher import string_hash
 from context import Context
 
 
-# Create and return an array of points based on the_geom string
-def create_polygon(multipolygon_string):
-    multipolygon_clean = multipolygon_string[16:-3]
-    points_with_spaces = multipolygon_clean.split(", ")
+# Create and return an array of points based on the neighborhood boundary string
+def create_polygon(neighborhood_boundary_string):
+    neighborhood_boundary = neighborhood_boundary_string[16:-3]
+    points_with_spaces = neighborhood_boundary.split(", ")
     points = []
     for point in points_with_spaces:
         coordinates = point.split(" ")
@@ -21,14 +21,14 @@ def create_polygon(multipolygon_string):
     return Polygon([[p.x, p.y] for p in points])
 
 
-multipolygon = udf(
-    lambda multipolygon_string: create_polygon(multipolygon_string), GeometryType()
+polygon = udf(
+    lambda neighborhood_boundary_string: create_polygon(neighborhood_boundary_string), GeometryType()
 )
 
 # Check if the latitude and longitude is in the neighborhood represented as a Polygon
 check_neighborhood_in_polygon = udf(
-    lambda latitude, longitude, polygon:
-    polygon.contains(Point(float(latitude), float(longitude))),
+    lambda latitude, longitude, polygon_object:
+    polygon_object.contains(Point(float(latitude), float(longitude))),
     BooleanType()
 )
 
@@ -43,7 +43,6 @@ class IncidentModernContext(Context):
     incident_modern_file = os.environ["CORE_CONF_fs_defaultFS"] \
                            + "/datasets/Police_Department_Incident_Reports__2018_to_Present.csv"
 
-    # TODO HBase table
     __catalog = ''.join("""{
             "table":{"namespace":"default", "name":"modern_incident_reports"},
             "rowkey":"key_analysis_neighborhood:key_incident_category:key_incident_datetime:key_row_id",
@@ -75,72 +74,65 @@ class IncidentModernContext(Context):
 
     def load_csv(self, session: SparkSession) -> DataFrame:
         # Read csv file
-        # The multiline config is necessary to support strings with line breaks in the csv file
-        # TODO FILE
-        incidents_modern_df = session.read.format("csv") \
+        incidents_df = session.read.format("csv") \
             .option("header", "true") \
             .option("multiline", "true") \
             .option("timestampFormat", "yyyy-MM-dd'T'HH:mm:ss.SSS") \
             .load(self.incident_modern_file) \
-            .limit(10)  # For testing purposes        # TODO file load
+            .limit(50)
 
         # Remove rows missing category or location information
-        incidents_modern_df = incidents_modern_df.where(
-            incidents_modern_df["Incident Category"].isNotNull() &
-            incidents_modern_df["Latitude"].isNotNull() &
-            incidents_modern_df["Longitude"].isNotNull()
+        incidents_df = incidents_df.where(
+            incidents_df["Incident Category"].isNotNull() &
+            incidents_df["Latitude"].isNotNull() &
+            incidents_df["Longitude"].isNotNull()
         )
-        incidents_modern_df.show(10, True)
 
-        # Read the neighborhood file and make a polygon based on the_geom column
+        # Read the neighborhood file from HDFS and create polygons based on the neighborhood boundaries
         sf_boundaries_file = os.environ["CORE_CONF_fs_defaultFS"] + "/datasets/SFFind_Neighborhoods.csv"
         sf_boundaries_df = session.read.format("csv").option("header", "true").load(sf_boundaries_file)
-        sf_boundaries_df = sf_boundaries_df.withColumn("the_geom", multipolygon(sf_boundaries_df["the_geom"]))
+        sf_boundaries_df = sf_boundaries_df.withColumn("the_geom", polygon(sf_boundaries_df["the_geom"]))
         sf_boundaries_df = sf_boundaries_df.select(
             sf_boundaries_df["the_geom"].alias("polygon"),
             sf_boundaries_df["name"].alias("analysis_neighborhood")
         )
 
         # Select the relevant columns
-        incidents_modern_df = incidents_modern_df.select(
+        incidents_df = incidents_df.select(
             string_to_hash("Row ID").alias("row_id"),
-            incidents_modern_df["Incident Category"].alias("incident_category"),
+            incidents_df["Incident Category"].alias("incident_category"),
             string_to_hash("Incident Category").alias("incident_category_id"),
-            incidents_modern_df["Incident Subcategory"].alias("incident_subcategory"),
-            unix_timestamp(to_timestamp("Incident Datetime", "yyyy/MM/dd hh:mm:ss a")).alias("incident_datetime"),
-            unix_timestamp(to_timestamp("Report Datetime", "yyyy/MM/dd hh:mm:ss a")).alias("report_datetime"),
-            incidents_modern_df["Incident ID"].alias("incident_id"),
-            incidents_modern_df["Resolution"].alias("resolution"),
-            incidents_modern_df["Intersection"].alias("intersection"),
-            incidents_modern_df["Latitude"].cast(DoubleType()).alias("latitude"),
-            incidents_modern_df["Longitude"].cast(DoubleType()).alias("longitude"),
-            incidents_modern_df["Report Type Code"].alias("report_type_code"),
-            incidents_modern_df["Report Type Description"].alias("report_type_description"),
-            incidents_modern_df["Incident Number"].alias("incident_number"),
-            incidents_modern_df["Police District"].alias("police_district"),
-            incidents_modern_df["Supervisor District"].alias("supervisor_district")
+            incidents_df["Incident Subcategory"].alias("incident_subcategory"),
+            unix_timestamp("Incident Datetime", "yyyy/MM/dd hh:mm:ss a").alias("incident_datetime"),
+            unix_timestamp("Report Datetime", "yyyy/MM/dd hh:mm:ss a").alias("report_datetime"),
+            incidents_df["Incident ID"].alias("incident_id"),
+            incidents_df["Resolution"].alias("resolution"),
+            incidents_df["Intersection"].alias("intersection"),
+            incidents_df["Latitude"].cast(DoubleType()).alias("latitude"),
+            incidents_df["Longitude"].cast(DoubleType()).alias("longitude"),
+            incidents_df["Report Type Code"].alias("report_type_code"),
+            incidents_df["Report Type Description"].alias("report_type_description"),
+            incidents_df["Incident Number"].alias("incident_number"),
+            incidents_df["Police District"].alias("police_district"),
+            incidents_df["Supervisor District"].alias("supervisor_district")
         )
-        incidents_modern_df.show(10, True)
 
-        # Join incident_mordern_df and sf_boundaries_df if latitude and longitude is in the polygon
-        incidents_modern_df = incidents_modern_df.join(
+        # Join incident_modern_df and sf_boundaries_df if latitude and longitude is in the polygon
+        incidents_df = incidents_df.join(
             sf_boundaries_df,
             check_neighborhood_in_polygon("latitude", "longitude", "polygon"),
             "cross"
         )
-        incidents_modern_df = incidents_modern_df.drop("polygon")
-        incidents_modern_df = incidents_modern_df.withColumn("analysis_neighborhood_id", string_to_hash(
-            incidents_modern_df["analysis_neighborhood"]))
-        incidents_modern_df.show(10, True)
 
-        return incidents_modern_df
+        incidents_df = incidents_df.drop("polygon")
+        incidents_df = incidents_df.withColumn("analysis_neighborhood_id", string_to_hash(
+            incidents_df["analysis_neighborhood"]))
 
+        incidents_df.show(100, True)
+        return incidents_df
 
     def load_hbase(self, session: SparkSession) -> DataFrame:
-        print("LOAD_HBASE")
         return session.read.options(catalog=self.__catalog).format(self._data_source_format).load()
 
-
     def save_hbase(self, df: DataFrame):
-         print("SAVE_HBASE")
-         df.write.options(catalog=self.__catalog, newtable="5").format(self._data_source_format).save()
+        df.write.options(catalog=self.__catalog, newtable="5").format(self._data_source_format).save()
