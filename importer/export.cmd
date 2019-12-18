815b1e3db712904ffb75b0c9ac03ff821c43974b
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2019-12-18 10:58:22 +0100

    Fixed import and export scripts

--- importer/export.cmd
+++ importer/export.cmd
@@ -1,19 +1,20 @@
 set /p dir="Enter export location: "
 
-docker build --rm -f "importer\Dockerfile" -t importer:latest "importer"
-
-docker run --rm --ip 172.200.0.1 --hostname exporter1 --name exporter1 -v datanode1 importer:latest
-docker run --rm --ip 172.200.0.2 --hostname exporter2 --name exporter2 -v datanode2 importer:latest
-docker run --rm --ip 172.200.0.3 --hostname exporter3 --name exporter3 -v datanode3 importer:latest
-docker run --rm --ip 172.200.0.4 --hostname exporter4 --name exporter4 -v zookeeper -v zookeeper_log importer:latest
+docker run --rm --ip 172.200.0.1 --hostname exporter1 --name exporter1 -v datanode1:/hadoop/dfs/data -t -d debian:9
+docker run --rm --ip 172.200.0.2 --hostname exporter2 --name exporter2 -v datanode2:/hadoop/dfs/data -t -d debian:9
+docker run --rm --ip 172.200.0.3 --hostname exporter3 --name exporter3 -v datanode3:/hadoop/dfs/data -t -d debian:9
+docker run --rm --ip 172.200.0.4 --hostname exporter4 --name exporter4 -v namenode:/hadoop/dfs/name -t -d debian:9
+docker run --rm --ip 172.200.0.5 --hostname exporter5 --name exporter5 -v zookeeper:/data -v zookeeper_log:/datalog -t -d debian:9
 
 docker cp exporter1:/hadoop/dfs/data %dir%/datanode1/
 docker cp exporter2:/hadoop/dfs/data %dir%/datanode2/
 docker cp exporter3:/hadoop/dfs/data %dir%/datanode3/
-docker cp exporter4:/data %dir%/zoo/
-docker cp exporter4:/datalog %dir%/datalog/
+docker cp exporter4:/hadoop/dfs/name %dir%/namenode/
+docker cp exporter5:/data %dir%/zooData/
+docker cp exporter5:/datalog %dir%/zooDatalog/
 
 docker stop exporter1
 docker stop exporter2
 docker stop exporter3
 docker stop exporter4
+docker stop exporter5
