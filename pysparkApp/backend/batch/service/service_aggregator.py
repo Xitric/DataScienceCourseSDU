6b09c88f5b69d8bd0684cb306f5a9640fa6936dc
Emil Nielsen <emilqz@gmail.com>
2019-12-20 23:51:51 +0100

    Clean up

--- pysparkApp/backend/batch/service/service_aggregator.py
+++ pysparkApp/backend/batch/service/service_aggregator.py
@@ -15,6 +15,30 @@ from mysql.connector import (connection)
 java_date_format = "yyyy-MM-dd"
 python_date_format = "%Y-%m-%d"
 
+
+def path_exist(path):
+    try:
+        rdd = spark.sparkContext.textFile(path)
+        rdd.take(1)
+        return True
+    except Py4JJavaError as _:
+        return False
+
+
+def insert_into_db(row: Row):
+    db_connection = mysql.connector.connect(host="mysql",
+                                            user="spark",
+                                            passwd="P18YtrJj8q6ioevT",
+                                            database="analysis_results")
+    query = "INSERT INTO service_cases_daily (neighborhood, category, rate, day) VALUES (%s,%s,%s,%s) ON DUPLICATE KEY UPDATE rate = VALUES(rate);"
+    insert_tuple = (row['neighborhood'], row['category'], row['rate'], row['day'])
+    cursor = db_connection.cursor(prepared=True)
+    cursor.execute(query, insert_tuple)
+    db_connection.commit()
+    cursor.close()
+    db_connection.close()
+
+
 if __name__ == "__main__":
     spark = SparkSession.builder.getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
@@ -24,32 +48,12 @@ if __name__ == "__main__":
     timestamp_file_path = os.environ["CORE_CONF_fs_defaultFS"] + "/aggregator_timestamps/service.csv"
     service_timestamp = 0
 
-
-    def path_exist(path):
-        try:
-            rdd = spark.sparkContext.textFile(path)
-            rdd.take(1)
-            return True
-        except Py4JJavaError as _:
-            return False
-
-
     if path_exist(timestamp_file_path):
         service_timestamp = spark.read.format("csv").load(timestamp_file_path).collect()[0][0]  # first value in the row
         print("file exists with timestamp: " + str(service_timestamp))
 
     context = ServiceRunningAggregationContext()
     df = context.load_hbase_timestamp(spark, int(service_timestamp))
-    df.show(10, True)
-    print(df.count())  # dette skal stige
-
-    # check file in hdfs, if a timetamp exists, take the timestamp-1 day and compute from there
-
-    # problem: beregner alt data fra de sidste mange år og gemmer det i mysql, når der kommer ny data er der
-    # ingen grund til at bergene alt data + det nye og så gemme det i mysql. Da vi har beregnet det gamle og gemt i mysql.
-    # Så gem hvor vi er nået til og bergen data for nuværende dag + den forrige og gem dette
-    # TODO: Use hdfs to store the last time from which data has been aggregated, so we do not need to aggregate
-    #  everything anew. Then the job can be run every day to support streaming
 
     # Prepare to normalize by population
     # [neighborhood, population_day]
@@ -77,21 +81,6 @@ if __name__ == "__main__":
     date_in_seconds = int((latest_date - datetime.utcfromtimestamp(0)).total_seconds())
     print(date_in_seconds)
 
-
-    def insert_into_db(row: Row):
-        db_connection = mysql.connector.connect(host="mysql",
-                                                user="spark",
-                                                passwd="P18YtrJj8q6ioevT",
-                                                database="analysis_results")
-        query = "INSERT INTO service_cases_daily (neighborhood, category, rate, day) VALUES (%s,%s,%s,%s) ON DUPLICATE KEY UPDATE rate = VALUES(rate);"
-        insert_tuple = (row['neighborhood'], row['category'], row['rate'], row['day'])
-        cursor = db_connection.cursor(prepared=True)
-        cursor.execute(query, insert_tuple)
-        db_connection.commit()
-        cursor.close()
-        db_connection.close()
-
-
     # Save to MySQL if there is no timestamp
     if service_timestamp == 0:
         daily_to_save.write.format('jdbc').options(
@@ -103,7 +92,6 @@ if __name__ == "__main__":
     else:
         daily_to_save.foreach(lambda row: insert_into_db(row))
 
-    daily_to_save.show(10, True)
     timestamp_df = spark.createDataFrame([(date_in_seconds,)], ["date"])  # spark expects a tuple
     timestamp_df.repartition(1).write.format("csv").mode("overwrite").save("/aggregator_timestamps/service.csv")
 
@@ -126,12 +114,6 @@ if __name__ == "__main__":
         .drop("category_id")
 
     # Convert many rows into one large row for each neighborhood that specifies rates for every category that month
-    # monthly_to_save = monthly_to_save.where(monthly_to_save["category"]
-    #                                         .isin(lit("Encampments"), lit("Illegal Postings"), lit("Street Defects")))
-    # monthly_to_save.explain()
-    # monthly_to_save = monthly_to_save.groupBy("neighborhood", "month") \
-    #     .pivot("category") \
-    #     .agg(first("rate").alias("rate"))
     monthly_to_save = monthly_to_save.groupBy("neighborhood", "month") \
         .pivot("category") \
         .agg(first("rate").alias("rate"))
