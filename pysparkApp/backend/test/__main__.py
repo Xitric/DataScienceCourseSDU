0a3b4c416cdfd2b1f8ba1992bef21d51dfb875ab
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2019-12-30 12:24:56 +0100

    Added frontend to compose and removed test files from backend

--- pysparkApp/backend/test/__main__.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql.session import SparkSession
-
-
-def run():
-    spark = SparkSession.builder \
-        .appName("SfDataImporter") \
-        .getOrCreate()
-    print(spark.sparkContext.parallelize([1, 2, 3]).count())
-    spark.stop()
