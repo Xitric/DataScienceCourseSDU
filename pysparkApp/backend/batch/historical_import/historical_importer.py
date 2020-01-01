138b82644d201770de80f2955b692bf0fbbf4307
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2020-01-01 16:17:57 +0100

    Bugfix and improvement of ingestion of historical incident reports

--- pysparkApp/backend/batch/historical_import/historical_importer.py
+++ pysparkApp/backend/batch/historical_import/historical_importer.py
@@ -1,4 +1,6 @@
-from pyspark.sql.functions import to_date, from_unixtime
+from datetime import datetime
+
+from pyspark.sql.functions import to_date, from_unixtime, sum
 from pyspark.sql.types import FloatType
 
 from batch.historical_import.data_importer import import_data
@@ -33,7 +35,8 @@ if __name__ == "__main__":
         .agg(sum("count").alias("count"))
 
     # Convert counts to rates normalized by population
-    daily_to_save = daily_df.withColumn("rate", (daily_df["count"] / daily_df["population_day"]).cast(FloatType())) \
+    daily_to_save = daily_df.orderBy("day", ascending=False) \
+        .withColumn("rate", (daily_df["count"] / daily_df["population_day"]).cast(FloatType())) \
         .drop("count") \
         .drop("population_day") \
         .drop("neighborhood_id") \
@@ -46,3 +49,14 @@ if __name__ == "__main__":
         dbtable='incident_cases_daily',
         user='spark',
         password='P18YtrJj8q6ioevT').mode('append').save()
+
+    # Update timestamp in HDFS to ensure that aggregation of modern incident reports does not overwrite the aggregation
+    # of historical reports
+    latest_date_from_df = daily_to_save.select(daily_to_save["day"]).collect()[0][0]
+    latest_date = datetime.strptime(str(latest_date_from_df), "%Y-%m-%d")
+    date_in_seconds = int((latest_date - datetime.utcfromtimestamp(0)).total_seconds())
+    print(date_in_seconds)
+
+    timestamp_df = spark.createDataFrame([(date_in_seconds,)], ["date"])  # Spark expects a tuple
+    timestamp_df.repartition(1).write.format("csv").mode("overwrite").save(
+        "/aggregator_timestamps/incident_cases.csv")
